b06dd9cee85504e09c9472353ddca8edaa8e7033
Anpanator <nickschradick@googlemail.com>
2020-05-20 01:51:20 +0200

    Handle case when JNC does not return a book but an error or redirect

--- jnc.sh
+++ jnc.sh
@@ -2,13 +2,13 @@
 set -e
 command -v jq > /dev/null
 command -v curl > /dev/null
-#Config: START
+# Config: START
 
 downloadTargetDirectory=~/Downloads/
 loginEmail='user@server.tld'
 loginPassword='somepassword'
 
-#Config: END
+# Config: END
 
 
 curTime=$(date -u +%s)
@@ -33,7 +33,7 @@ authorizationToken=$(jq -r '.["id"]' "$loginResponseFile")
 userId=$(jq -r '.["user"]["id"]' "$loginResponseFile")
 userName=$(jq -r '.["user"]["username"]' "$loginResponseFile")
 
-#Get access tokens and owned books of this account
+# Get access tokens and owned books of this account
 detailsRequestUrl='https://api.j-novel.club/api/users/'
 detailsRequestUrl+=$userId
 detailsRequestUrl+='?filter={"include":[{"ownedBooks":"serie"}]}'
@@ -41,7 +41,7 @@ detailsRequestUrl+='?filter={"include":[{"ownedBooks":"serie"}]}'
 curl --globoff --silent --request 'GET' "$detailsRequestUrl" --header "Authorization: $authorizationToken" \
 > "$rawAccountDetailsFile"
 
-#Parse the "ownedBooks" section of the raw account information for the relevant data, pipe-separated
+# Parse the "ownedBooks" section of the raw account information for the relevant data, pipe-separated
 jq -r '.["ownedBooks"][] | .id + "|" + .publishingDate + "|" + .title' "$rawAccountDetailsFile" > "$ownedBooksList"
 
 cd "$downloadTargetDirectory"
@@ -53,19 +53,21 @@ while read line;
   if [ $curTime -gt $bookTime ] &&  ! grep -Fxq "$bookId" "$downloadedBooksList";
     then
       set +e
-      echo "$bookId 	 $bookTime 	 $bookTitle"
+      echo "$bookTitle 	 $bookId 	 $bookTime"
       downloadLink="https://api.j-novel.club/api/volumes/${bookId}/getpremiumebook?userId=${userId}&userName=${userName}&access_token=$authorizationToken"
-      curl -O -J -f --globoff "$downloadLink"
-      if [ $? -eq 0 ]
+      statuscode=$(curl --remote-name --remote-header-name --fail --globoff --silent --write-out "%{http_code}" "$downloadLink")
+      if [ "$statuscode" -eq "200" ]
         then
         echo $bookId >> "$downloadedBooksList"
+        else
+        echo "Returned HTTP $statuscode. Book was not saved!"
       fi
       set -e
     fi
 done < "$ownedBooksList"
 cd - > /dev/null
 
-#Log out to invalidate access token
+# Log out to invalidate access token
 curl --silent --request 'POST' 'https://api.j-novel.club/api/users/logout' \
 --header "Authorization: $authorizationToken"
 echo 'Finished downloading and logged out'
