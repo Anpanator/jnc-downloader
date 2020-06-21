f77ed0e4bc6e85fe1167327776980831250628e4
Anpanator <nickschradick@googlemail.com>
2020-06-21 22:04:35 +0200

    Print new available volume titles of followed series

--- jnc.py
+++ jnc.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+
+import json
 import sys
 import os
 import csv
@@ -13,8 +15,14 @@ download_target_dir = '~/Downloads/'
 downloaded_books_list_file = '~/.downloadedJncBooks.csv'  # Format book_id + \t + title_slug
 owned_series_file = '~/.jncOwnedSeries.csv'  # Format series_title_slug + \t + followed (boolean)
 
-
 # Config: END
+
+try:
+    jnclient = JNClient(login_email, login_pw)
+except JNCApiError as err:
+    print(err)
+    sys.exit(1)
+
 
 def read_owned_series_file(file_path):
     """:return dictionary like {"title_slug": boolean}"""
@@ -33,6 +41,18 @@ def store_owned_series_file(file_path, data):
         series_csv_writer.writerows(data.items())
 
 
+def download_book(book_id, book_title, downloaded_books):
+    try:
+        book_content = jnclient.download_book(book_id)
+
+        downloaded_books[book_id] = book_title
+
+        with open(book_file_path, mode='wb') as f:
+            f.write(book_content)
+    except JNCApiError as err:
+        print(err)
+
+
 download_target_dir = os.path.expanduser(download_target_dir)
 
 downloaded_books_list_file = os.path.expanduser(downloaded_books_list_file)
@@ -42,12 +62,6 @@ if not os.path.isfile(downloaded_books_list_file):
 owned_series_file = os.path.expanduser(owned_series_file)
 if not os.path.isfile(owned_series_file):
     open(owned_series_file, 'a').close()
-
-try:
-    jnclient = JNClient(login_email, login_pw)
-except JNCApiError as err:
-    print(err)
-    sys.exit(1)
 
 # overwrite credentials to make sure they're not used later
 login_email = None
@@ -81,7 +95,7 @@ if os.path.exists(downloaded_books_list_file):
     with open(downloaded_books_list_file, mode='r', newline='') as f:
         downloaded_book_ids = [row[0] for row in csv.reader(f, delimiter='\t')]
 
-downloaded_books = []
+downloaded_books = {}
 preordered_books = []
 
 for book in owned_books:
@@ -90,7 +104,7 @@ for book in owned_books:
     book_title = book['title']
 
     if book_id in downloaded_book_ids:
-        downloaded_books.append([book_id, book_title])
+        downloaded_books[book_id] = book_title
         continue
 
     if book_time > cur_time:
@@ -102,24 +116,32 @@ for book in owned_books:
     book_file_name = book['titleslug'] + '.epub'
     book_file_path = os.path.join(download_target_dir, book_file_name)
 
-    try:
-        book_content = jnclient.download_book(book_id)
+    download_book(book_id, book_title, downloaded_books)
 
-        downloaded_books.append([book_id, book_title])
+books_to_order = {}
+# Check for new volumes in followed series
+for series_title_slug in series_follow_states:
+    if series_follow_states[series_title_slug]:
+        series_info = jnclient.get_series_info(series_title_slug)
+        for volume in series_info['volumes']:
+            # Check if the volume is not yet owned
+            if not any(d['id'] == volume['id'] for d in owned_books):
+                books_to_order[volume['id']] = {'titleslug': volume['titleslug'], 'title': volume['title']}
 
-        with open(book_file_path, mode='w', newline='') as f:
-            f.write(book_content)
-    except JNCApiError as err:
-        print(err)
-
+# Save list of books that were downloaded
 with open(downloaded_books_list_file, mode='w', newline='') as f:
     csv_writer = csv.writer(f, delimiter='\t')
-    csv_writer.writerows(downloaded_books)
+    csv_writer.writerows(downloaded_books.items())
 
+# Print unreleased preorders
 if len(preordered_books) > 0:
     print('\nBooks scheduled to be released after %s' % cur_time)
 
     for book_title, book_id, book_time in preordered_books:
         print('%s  %s' % (book_time, book_title))
+
+print('\nThe following new books of series you follow can be ordered:')
+for book_id in books_to_order:
+    print(books_to_order[book_id]['title'])
 
 del jnclient
